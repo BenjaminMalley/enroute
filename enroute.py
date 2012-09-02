2a1320da38cee1a7557dc5ffda010607086bda14
BenjaminMalley <benjamin.malley@gmail.com>
2012-09-01 23:41:54 -0400

    formatting changes

--- enroute.py
+++ enroute.py
@@ -25,45 +25,36 @@ def verify_response(resp, content):
 			log.write(request.url+"\n")
 			log.write("".join(["twitter response: ", str(resp), "\n"]))
 			log.write("".join(["twitter content: ", content, "\n"]))
-
-	if resp['status'] != '200':
+	if resp["status"] != "200":
 		session.pop("access_token", None)
-		session.pop('request_token', None)
-		flash('Bad response from Twitter')	
-		return redirect(url_for('index'))	
+		session.pop("request_token", None)
+		flash("Bad response from Twitter")	
+		return redirect(url_for("index"))	
 	else:
 		return None
 
-@app.route('/', methods=['GET',])
+@app.route("/", methods=["GET",])
 def index():
-	return render_template('index.html')
+	return render_template("index.html")
 
-@app.route('/signin/')
+@app.route("/signin/")
 def twitter_signin():
-	#Step 1:
 	resp, content = app.client.request(app.auth_url+"request_token", "POST",
 		body=urlencode({"oauth_callback": app.site_url+url_for("twitter_authenticated")}))
 	verify_response(resp, content)
-	
-	#Step 2:
-	session['request_token'] = dict(urlparse.parse_qsl(content))
-	
-	#Step 3:
+	session["request_token"] = dict(urlparse.parse_qsl(content))
 	return redirect("{0}?oauth_token={1}".format(app.auth_url+"authorize",
 							session["request_token"]["oauth_token"]))
 							
-@app.route('/authenticated/')
+@app.route("/authenticated/")
 def twitter_authenticated():
-	if 'request_token' in session:
-		#Step 1:
-		client = oauth.Client(app.consumer, oauth.Token(session['request_token']['oauth_token'],
-			session['request_token']['oauth_token_secret']))
-			
-		#Step 2:
+	if "request_token" in session:
+		client = oauth.Client(app.consumer, oauth.Token(session["request_token"]["oauth_token"],
+			session["request_token"]["oauth_token_secret"]))
 		resp, content = client.request(app.auth_url+"access_token", "GET")
 		verify_response(resp, content)
 		session["access_token"] = dict(urlparse.parse_qsl(content))
-		return render_template('route.html', maps_api_key=config.maps_api_key)
+		return render_template("route.html", maps_api_key=config.maps_api_key)
 	else:
 		return "You must have an active session"
 
@@ -71,10 +62,9 @@ def twitter_authenticated():
 def send_tweet():
 	if "access_token" in session:
 		client = oauth.Client(app.consumer,
-			oauth.Token(key=session['access_token']['oauth_token'],
-			secret=session['access_token']['oauth_token_secret']))
-		
-		resp, content = client.request("https://api.twitter.com/1/statuses/update.json", "POST",
+			oauth.Token(key=session["access_token"]["oauth_token"],
+			secret=session["access_token"]["oauth_token_secret"]))
+		resp, content = client.request(config.tweet_url, "POST",
 			body=urlencode({"status":
 			"I started a trip! Track my progress at {0}track/{1}".format(
 			app.site_url, session["access_token"]["screen_name"])}))
@@ -123,18 +113,18 @@ def route():
 	return 'OK'
 '''
 
-@app.route('/track/<user>/')
+@app.route("/track/<user>/")
 def track_user(user):
-	if not app.cache.exists(':'.join([user,'dur'])):
-		return make_response('nothing here', 404)
-	return render_template('track.html', maps_api_key=config.maps_api_key, user=user)
+	if not app.cache.exists(":".join([user,"dur"])):
+		return make_response("nothing here", 404)
+	return render_template("track.html", maps_api_key=config.maps_api_key, user=user)
 
-@app.route('/loc/<user>/')
+@app.route("/loc/<user>/")
 def get_location(user):
-	d = dict([(i,app.cache.get(':'.join([user,i])))
-		for i in ('lat','lng','dest','dur')])
+	d = dict([(i,app.cache.get(":".join([user,i])))
+		for i in ("lat","lng","dest","dur")])
 	return json.dumps(d)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	app.run(debug=True)
